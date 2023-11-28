59b947ae97f119fdadf7193a283f89bae8281eb3
Eliott COUTAZ <eliott.coutaz@edu.devinci.fr>
2023-11-29 00:03:27 +0100

    first change

--- Main.py
+++ Main.py
@@ -4,41 +4,62 @@ import matplotlib.pyplot as plt
 from datetime import datetime, timedelta
 
 # Twitter API credentials
-api_key = 'pmE3eJn3wAncZboVybljobSPf'
-api_secret = 'VsMgrw5IJ3w3dDtQOpnfedvKpVkxGMIvFY3gp3zcWxfQdqjLgR'
-access_token = '1621506331906830349-KwapThh9sa9X1sLTsZYl46WtpCRoAx'
-access_token_secret = 'V8clCdfEOU2Uo2o1omFzuJ27fcpQjJuoN1LJJDgYgGUkJ'
+api_key = '*****'
+api_secret = '*****'
+access_token = '*****-*****'
+access_token_secret = '******'
 
 # Authenticate with Twitter
 auth = tweepy.OAuthHandler(api_key, api_secret)
 auth.set_access_token(access_token, access_token_secret)
 api = tweepy.API(auth)
 
-# Hashtag and Time Frame
-hashtag = "#YourHashtag"
-since_time = (datetime.now() - timedelta(hours=1)).strftime('%Y-%m-%d %H:%M:%S')
+# Specific Hashtags and Keywords for the Football Game
+team1 = "#Team1"
+team2 = "#Team2"
+match_hashtag = "#MatchHashtag"
 
-# Fetching Tweets
-tweets = api.search_tweets(q=hashtag, count=2, since=since_time)
+# Game Result (Manually inputted or fetched from another source)
+game_result = "Team1 wins"  # Example: "Team1 wins", "Team2 wins", "Draw"
+
+# Time Frame for Before and After the Game
+before_game_time = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d %H:%M:%S')  # Example: 24 hours before now
+after_game_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+
+# Fetching Tweets Before the Game
+before_game_tweets = api.search_tweets(q=f"{team1} OR {team2} OR {match_hashtag}", count=100, until=before_game_time)
+
+# Fetching Tweets After the Game
+after_game_tweets = api.search_tweets(q=f"{team1} OR {team2} OR {match_hashtag}", count=100, since=after_game_time)
+
+# Function for Sentiment Analysis
+def analyze_sentiments(tweets):
+    sentiments = {"positive": 0, "neutral": 0, "negative": 0}
+    for tweet in tweets:
+        analysis = TextBlob(tweet.text)
+        polarity = analysis.sentiment.polarity
+        if polarity > 0:
+            sentiments["positive"] += 1
+        elif polarity == 0:
+            sentiments["neutral"] += 1
+        else:
+            sentiments["negative"] += 1
+    return sentiments
 
 # Sentiment Analysis
-sentiments = {"positive": 0, "neutral": 0, "negative": 0}
-for tweet in tweets:
-    analysis = TextBlob(tweet.text)
-    polarity = analysis.sentiment.polarity
-    if polarity > 0:
-        sentiments["positive"] += 1
-    elif polarity == 0:
-        sentiments["neutral"] += 1
-    else:
-        sentiments["negative"] += 1
+before_sentiments = analyze_sentiments(before_game_tweets)
+after_sentiments = analyze_sentiments(after_game_tweets)
 
 # Plotting
-labels = sentiments.keys()
-sizes = sentiments.values()
+fig, axes = plt.subplots(nrows=1, ncols=2, figsize=(10, 5))
+axes[0].bar(before_sentiments.keys(), before_sentiments.values())
+axes[0].set_title('Sentiments Before Game')
+axes[0].set_xlabel('Sentiments')
+axes[0].set_ylabel('Number of Tweets')
 
-plt.bar(labels, sizes)
-plt.xlabel('Sentiments')
-plt.ylabel('Number of Tweets')
-plt.title('Sentiment Analysis of Tweets')
+axes[1].bar(after_sentiments.keys(), after_sentiments.values())
+axes[1].set_title('Sentiments After Game')
+axes[1].set_xlabel('Sentiments')
+
+plt.suptitle(f'Sentiment Analysis of Tweets for {match_hashtag} ({game_result})')
 plt.show()
